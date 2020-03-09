127ed6138fc43a6f49d793916ee205a81f6a8ce4
Phu Huynh <phu@filesoft.tech>
2020-03-09 14:14:38 +0700

    Complete Simple Progress bar

--- SimpleProgressBar/ContentView.swift
+++ SimpleProgressBar/ContentView.swift
@@ -9,8 +9,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var simpleProgess: CGFloat = 0.0
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("\(Int(self.simpleProgess*100))%")
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .foregroundColor(.gray)
+                    .frame(width: 400, height: 20)
+                    .cornerRadius(20)
+                
+                Rectangle()
+                    .foregroundColor(.blue)
+                    .frame(width: self.simpleProgess * 400, height: 20)
+                    .cornerRadius(20)
+            }
+            
+            Button(action: { self.startTimer() }) {
+                Text("Start Timer")
+            }
+        }
+    }
+    
+    func startTimer() {
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { (timer) in
+            if self.simpleProgess >= 1.0 {
+                timer.invalidate()
+            } else {
+                self.simpleProgess += 0.01
+            }
+        }
     }
 }
 
